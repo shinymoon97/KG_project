0f054f6b606909420390cb16eb98b15ffa076ed1
shinymoon97 <shinymoon97@naver.com>
2025-06-01 14:46:02 +0900

    Update db.sql

--- CafeOrderApp/src/main/resources/static/db.sql
+++ CafeOrderApp/src/main/resources/static/db.sql
@@ -2,10 +2,10 @@ CREATE TABLE `order` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `coffee_type` VARCHAR(255),
   `quantity` INT,
+  `pickup_time` VARCHAR(255),
   `takeout` BIT,
-  `delivery_time` VARCHAR(255),
   `drive_thru` BIT,
-  `estimated_pickup_time` VARCHAR(255),
+  `estimated_pickup_time` VARCHAR(255)
   PRIMARY KEY (`id`)
 );
 
