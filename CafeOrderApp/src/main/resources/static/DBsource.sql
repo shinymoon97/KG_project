a8d2572b592c105778f1d4ac27120e4cad86a624
shinymoon97 <shinymoon97@naver.com>
2025-06-01 14:48:08 +0900

    Create DBsource.sql

---
+++ CafeOrderApp/src/main/resources/static/DBsource.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `order` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `coffee_type` VARCHAR(255),
+  `quantity` INT,
+  `pickup_time` VARCHAR(255),
+  `takeout` BIT,
+  `drive_thru` BIT,
+  `estimated_pickup_time` VARCHAR(255)
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE seat_status (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  total_seats INT NOT NULL,
+  available_seats INT NOT NULL
+);
