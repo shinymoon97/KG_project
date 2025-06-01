befef3a6e282efd6f4a5835519d8c19dd332b22f
shinymoon97 <shinymoon97@naver.com>
2025-06-01 15:18:11 +0900

    Update DBsource.sql

--- CafeOrderApp/src/main/resources/static/DBsource.sql
+++ CafeOrderApp/src/main/resources/static/DBsource.sql
@@ -1,16 +1,19 @@
-CREATE TABLE `order` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `coffee_type` VARCHAR(255),
-  `quantity` INT,
-  `pickup_time` VARCHAR(255),
-  `takeout` BIT,
-  `drive_thru` BIT,
-  `estimated_pickup_time` VARCHAR(255)
-  PRIMARY KEY (`id`)
+CREATE DATABASE coffeeDB;
+USE coffeeDB;
+
+CREATE TABLE order (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    coffee_type VARCHAR(255),
+    quantity INT,
+    pickup_time VARCHAR(255),
+    takeout BIT,
+    drive_thru BIT,
+    estimated_pickup_time VARCHAR(255)
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE seat_status (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  total_seats INT NOT NULL,
-  available_seats INT NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    total_seats INT NOT NULL,
+    available_seats INT NOT NULL
 );
