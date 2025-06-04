ef7b07236a5d4376f3f44a6b3ba17f04994d2e49
shinymoon97 <shinymoon97@naver.com>
2025-06-04 14:08:26 +0900

    Update DBsource.sql

--- CafeOrderApp/src/main/resources/static/DBsource.sql
+++ CafeOrderApp/src/main/resources/static/DBsource.sql
@@ -1,5 +1,14 @@
-CREATE DATABASE cafeapp;
+CREATE DATABASE cafeapp; -- 먼저 이 DB를 만든 후 Spring Boot 실행 (안하면 오류발생)
 USE cafeapp;
+
+CREATE TABLE user_info (
+	id VARCHAR(255), -- 자동 생성되는 PK
+    username VARCHAR(255), -- 사용자 아이디
+	pwd VARCHAR(255), -- 비밀번호
+	name VARCHAR(255), -- 이름
+	email VARCHAR(255), -- 이메일
+	PRIMARY KEY (id)
+);
 
 CREATE TABLE order_menu (
     id BIGINT NOT NULL AUTO_INCREMENT,
