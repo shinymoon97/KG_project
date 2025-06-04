7a4ecffbbae50ea331eb4520a10a3712f22452c9
shinymoon97 <shinymoon97@naver.com>
2025-06-04 16:26:19 +0900

    Update UserController.java

--- CafeOrderApp/src/main/java/com/example/demo/user/controller/UserController.java
+++ CafeOrderApp/src/main/java/com/example/demo/user/controller/UserController.java
@@ -1,51 +1,43 @@
 package com.example.demo.user.controller;
 
-import com.example.demo.user.entity.UserForm;
+import com.example.demo.user.entity.User;
 import com.example.demo.user.service.UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
-    
-    @GetMapping("/")
-    public String index() {
-        return "index"; // templates/index.html 로 이동
-    }
-    
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/register")
-    public String register() {
-        return "register"; // templates/register.html 로 이동
+    public String showRegisterForm() {
+        return "register";
     }
-    
+
     @PostMapping("/register")
-    public String register(@ModelAttribute UserForm user) {
-        userService.save(user);  // DB에 저장
-        return "redirect:/success";  // 회원가입 성공 후 이동할 경로
+    public String register(User user) {
+        userService.register(user);
+        return "redirect:/login";
     }
-    
-    @PostMapping("/success")
-    public String success() {
-        return "success"; // templates/success.html 로 이동
-    }
-    
+
     @GetMapping("/login")
-    public String login() {
-        return "login"; // templates/login.html 로 이동
+    public String showLoginForm() {
+        return "login";
     }
-    
+
     @PostMapping("/login")
-    public String login(@ModelAttribute UserForm user) {
-        // userService.save(user);  // DB에 저장
-        return "redirect:/welcome";  // 로그인 성공 후 이동할 경로
-    }
-    
-    @PostMapping("/welcome")
-    public String welcome() {
-        return "welcome"; // templates/welcome.html 로 이동
+    public String login(@RequestParam String email,
+                        @RequestParam String password,
+                        Model model) {
+        return userService.login(email, password)
+                .map(user -> {
+                    model.addAttribute("user", user);
+                    return "welcome";
+                })
+                .orElse("login");
     }
 }
