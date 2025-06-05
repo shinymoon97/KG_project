83b85d873fb4ced1fb4353d2c9f5207762673781
shinymoon97 <shinymoon97@naver.com>
2025-06-05 14:02:49 +0900

    Update SeatRepository.java

--- CafeOrderApp/src/main/java/com/example/demo/seat/repository/SeatRepository.java
+++ CafeOrderApp/src/main/java/com/example/demo/seat/repository/SeatRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.seat.repository;
 
-import java.util.List;
-
+import com.example.demo.seat.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.seat.entity.Seat;
-
 public interface SeatRepository extends JpaRepository<Seat, Long> {
-    List<Seat> findByAvailableTrue();
 }
