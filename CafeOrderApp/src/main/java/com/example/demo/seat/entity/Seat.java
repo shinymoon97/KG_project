0afc735802522d672e648b2ab754b3fa8182eca4
shinymoon97 <shinymoon97@naver.com>
2025-06-05 14:02:05 +0900

    Update Seat.java

--- CafeOrderApp/src/main/java/com/example/demo/seat/entity/Seat.java
+++ CafeOrderApp/src/main/java/com/example/demo/seat/entity/Seat.java
@@ -1,29 +1,24 @@
 package com.example.demo.seat.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "seat_status")  // 여기서 데이터가 저장될 테이블 이름 설정
+@Table(name = "seat_status")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Seat {
-	
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String total_seats;
-    private String available_seats;
-    private boolean available;
+    private String seatNumber;
 
-    // getters, setters
+    private String reservedBy;
+
+    private LocalDate reserveDate;
 }
