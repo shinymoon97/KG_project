0aa0da9e21fa501c36b04f3d3db6a12e45ecef2e
shinymoon97 <shinymoon97@naver.com>
2025-06-05 14:03:48 +0900

    Delete CafeOrderApp/src/main/java/com/example/demo/seat/dto directory

--- CafeOrderApp/src/main/java/com/example/demo/seat/dto/SeatDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.seat.dto;
-public class SeatDTO {
-    private Long id;
-    private String seatNumber;
-    private String type;       // 예: "window", "booth", "bar"
-    private boolean available; // 예: true=이용 가능
-}
